16dcfe41fac9d6bf42a1a2ca891edfeedacadfca
izan <ifabregat@inscamidemar.cat>
2023-10-17 20:16:20 +0200

    CUMMIT

--- Prova1.java
+++ Prova1.java
@@ -1,4 +1,11 @@
-int Wasa = 0
-int Epico = 5
-if (Wasa = Epico)
-  system.out.print("El fin se acerca")
+public class Prova1 {
+  public static void main(String[] args) {
+  
+
+int Wasa = 4;
+int Epico = 5;
+if (Wasa > Epico)
+  System.out.println("El fin se acerca");
+  
+  } 
+} 
